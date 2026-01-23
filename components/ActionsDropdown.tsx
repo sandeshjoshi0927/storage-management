99fa465a9f9ffa6e4b41cb6edb5810e7a4cc424a
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2026-01-23 07:40:26 +0545

    rename file

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -22,13 +22,16 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import { renameFile } from "@/lib/actions/file.actions";
 
 const ActionsDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [action, setAction] = useState<ActionType | null>(null);
-  const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState(file.name);
+  const path = usePathname();
 
   const closeAllModals = () => {
     setIsModalOpen(false);
@@ -38,7 +41,25 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
     // setEmails([]);
   };
 
-  const handleAction = async () => {};
+  const handleAction = async () => {
+    if (!action) return;
+    setIsLoading(true);
+
+    let success = false;
+    const actions = {
+      rename: () =>
+        renameFile({ fileId: file.$id, name, extension: file.extension, path }),
+
+      share: () => console.log("Share"),
+      delete: () => console.log("Delete"),
+    };
+
+    success = await actions[action.value as keyof typeof actions]();
+
+    if (success) closeAllModals();
+
+    setIsLoading(false);
+  };
 
   const renderDialogContent = () => {
     if (!action) return null;
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -97,3 +97,31 @@ export const getFiles = async () => {
     handleError(error, "Failed to get the files.");
   }
 };
+
+export const renameFile = async ({
+  fileId,
+  name,
+  extension,
+  path,
+}: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const newName = `${name}.${extension}`;
+
+    const updatedFile = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesTableId,
+      fileId,
+      {
+        name: newName,
+      },
+    );
+
+    revalidatePath(path);
+
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Failed to rename the file.");
+  }
+};
