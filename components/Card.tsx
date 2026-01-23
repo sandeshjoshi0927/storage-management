feae06c3e17c13fcced31a91a0afd88304fe98f7
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2026-01-23 07:19:07 +0545

    action dropdown menu

--- components/Card.tsx
+++ components/Card.tsx
@@ -19,7 +19,7 @@ const Card = ({ file }: { file: Models.Document }) => {
           imageClassName="!size-11"
         />
         <div className="flex flex-col items-end justify-between">
-          <ActionsDropdown />
+          <ActionsDropdown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
