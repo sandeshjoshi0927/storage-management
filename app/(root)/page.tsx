22c6ff9d7edbf290ecf0d49913f996275f0b5dfd
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2026-01-27 07:22:06 +0545

    sorting'

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
-export default function Home() {
+import Card from "@/components/Card";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+
+export default async function Home() {
+  const files = await getFiles({ types: [] });
+
   return (
     <div>
-      <h1 className="flex-center h-screen">
-        StoreIt - The only storage solution you need.
-      </h1>
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <Card key={file.$id} file={file} />
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
     </div>
   );
 }
